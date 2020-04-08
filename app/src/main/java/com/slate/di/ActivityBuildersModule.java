10c4f4b8de6c0e22f421df62c8612a283b7e3564
stunpayne <sunnykumar@Sunnys-MacBook-Pro.local>
2020-04-08 00:25:10 -0500

    Move Sign In button to Fragment

--- app/src/main/java/com/slate/di/ActivityBuildersModule.java
+++ app/src/main/java/com/slate/di/ActivityBuildersModule.java
@@ -2,12 +2,15 @@ package com.slate.di;
 
 import com.slate.activity.MainActivity;
 
+import com.slate.fragments.SignInFragment;
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
 public abstract class ActivityBuildersModule {
 
-  @ContributesAndroidInjector
+  @ContributesAndroidInjector(
+      modules = {MainFragmentBuildersModule.class}
+  )
   abstract MainActivity contributeMainActivity();
 }
---
+++ app/src/main/java/com/slate/fragments/SignInFragment.java
@@ -0,0 +1,45 @@
+package com.slate.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.slate.activity.R;
+import com.slate.user.SignInHandler;
+import dagger.android.support.DaggerFragment;
+import javax.inject.Inject;
+
+public class SignInFragment extends DaggerFragment {
+
+  private static final String TAG = SignInFragment.class.getSimpleName();
+
+  private final SignInHandler signInHandler;
+
+  @Inject
+  public SignInFragment(SignInHandler signInHandler) {
+    this.signInHandler = signInHandler;
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    super.onCreateView(inflater, container, savedInstanceState);
+    Toast.makeText(getActivity(), "Sign In Fragment", Toast.LENGTH_SHORT).show();
+    return inflater.inflate(R.layout.signin_fragment, container, false);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    setupGoogleSignIn();
+  }
+
+  private void setupGoogleSignIn() {
+    getActivity().findViewById(R.id.sign_in_button)
+        .setOnClickListener(signInHandler.createSignInButtonListener(getActivity()));
+  }
+}
